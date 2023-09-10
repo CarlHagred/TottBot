fad9b9e76d55d5d564428a2eaf132addbfa4a8e5
CarlHagred <carl.hagred@gmail.com>
2023-09-10 12:22:21 +0200

    init

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+const prefix string = "tott"
+
+func main() {
+	godotenv.Load()
+	os.Getenv("BOT_TOKEN")
+	sess, err := discordgo.New("Bot MTE0OTk5NTYzMTg1NTgxMjYzOA.GpzBGW.L2YYAuMTUzVDzr_ftLlYgH_pIb4K9i7SyIg1t8")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sess.AddHandler(inputHandler)
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = sess.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer sess.Close()
+
+	fmt.Println("the bot is online")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
+
+func inputHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	args := strings.Split(m.Content, " ")
+
+	if args[0] != prefix {
+		return
+	}
+
+	if args[1] == "hello" {
+		s.ChannelMessageSend(m.ChannelID, "world!")
+	}
+}
