d144494b96763d5bae8353003bb7b5a048f616eb
CarlHagred <carl.hagred@gmail.com>
2023-09-26 22:11:01 +0200

    this is it

--- go.mod
+++ go.mod
@@ -3,9 +3,25 @@ module tottbot
 go 1.20
 
 require (
+	github.com/Andreychik32/ytdl v1.0.4 // indirect
+	github.com/VividCortex/ewma v1.1.1 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
+	github.com/antchfx/jsonquery v1.1.4 // indirect
+	github.com/antchfx/xpath v1.1.7 // indirect
 	github.com/bwmarrin/discordgo v0.27.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/hajimehoshi/oto v1.0.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kkdai/youtube v1.2.4 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/rs/zerolog v1.19.0 // indirect
+	github.com/vbauerster/mpb/v5 v5.2.4 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
+	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
+	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
--- main.go
+++ main.go
@@ -2,42 +2,48 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
 
+	"github.com/Andreychik32/ytdl"
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
-const prefix string = "tott"
+const (
+	prefix = "tott"
+)
 
 func main() {
 	godotenv.Load()
 	token := os.Getenv("BOT_TOKEN")
-	sess, err := discordgo.New("Bot " + token)
+	discord, err := discordgo.New("Bot " + token)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	sess.AddHandler(inputHandler)
+	discord.AddHandler(inputHandler)
 
-	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+	discord.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
 
-	err = sess.Open()
+	err = discord.Open()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error opening connection:", err)
 	}
 
-	defer sess.Close()
+	defer discord.Close()
 
-	fmt.Println("the bot is online")
+	fmt.Println("Bot is now online. Press CTRL+C to exit.")
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
+	_ = discord.Close()
 }
 
 func inputHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -51,7 +57,120 @@ func inputHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	if args[1] == "hello" {
-		s.ChannelMessageSend(m.ChannelID, "world!")
+	if args[1] == "August" {
+		s.ChannelMessageSend(m.ChannelID, "Den killen är kort!")
 	}
+
+	if args[1] == "play" {
+		voiceChannelID, err := getVoiceChannelID(s, m.GuildID, m.Author.ID)
+		if err != nil {
+			fmt.Println("Error finding user's voice channel:", err)
+			return
+		}
+
+		if voiceChannelID == "" {
+			fmt.Println("User is not in a voice channel.")
+			return
+		}
+
+		audioURL, err := extractYouTubeAudioURL("https://www.youtube.com/watch?v=y6120QOlsfU")
+		if err != nil {
+			fmt.Println("Error extracting YouTube audio URL:", err)
+			return
+		}
+
+		vc, err := s.ChannelVoiceJoin(m.GuildID, voiceChannelID, false, false)
+		if err != nil {
+			fmt.Println("Error joining voice channel:", err)
+			return
+		}
+		defer vc.Disconnect()
+
+		err = downloadAndPlayAudio(vc, audioURL)
+		if err != nil {
+			fmt.Println("Error playing audio:", err)
+			return
+		}
+	}
+}
+
+func getVoiceChannelID(s *discordgo.Session, guildID, userID string) (string, error) {
+	guild, err := s.Guild(guildID)
+	if err != nil {
+		return "", err
+	}
+
+	for _, vs := range guild.VoiceStates {
+		if vs.UserID == userID {
+			return vs.ChannelID, nil
+		}
+	}
+
+	return "", nil
+}
+
+func downloadAndPlayAudio(vc *discordgo.VoiceConnection, audioURL string) error {
+	resp, err := http.Get(audioURL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	done := make(chan bool)
+	errChan := make(chan error)
+
+	go func() {
+		err := vc.Speaking(true)
+		if err != nil {
+			errChan <- err
+			return
+		}
+
+		_, err = io.Copy(vc, resp.Body)
+		if err != nil {
+			errChan <- err
+			return
+		}
+
+		err = vc.Speaking(false)
+		if err != nil {
+			errChan <- err
+			return
+		}
+
+		done <- true
+	}()
+
+	select {
+	case <-done:
+		return nil
+	case err := <-errChan:
+		return err
+	}
+}
+
+func extractYouTubeAudioURL(ytURL string) (string, error) {
+	// Create a new video info fetcher
+	vidInfo, err := ytdl.GetVideoInfo(ytURL)
+	if err != nil {
+		return "", err
+	}
+
+	// Choose the audio format with the highest quality
+	var audioFormat *ytdl.Format
+	for _, format := range vidInfo.Formats {
+		if format.AudioEncoding == nil {
+			continue
+		}
+
+		if audioFormat == nil || format.AudioQuality > audioFormat.AudioQuality {
+			audioFormat = format
+		}
+	}
+
+	if audioFormat == nil {
+		return "", fmt.Errorf("no audio format found")
+	}
+
+	return audioFormat.URL, nil
 }
